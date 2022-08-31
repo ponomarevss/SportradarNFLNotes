e3695a3c83bfc7f6a89a8c07deb02e263501fe65
ponomarevss <s-siberia@yandex.ru>
2022-08-31 16:47:31 +0500

    mvp to mvvm refactoring: vm survives its fragment

--- app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/ui/activity/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/ui/activity/MainActivity.kt
@@ -1,20 +1,16 @@
 package ru.geekbrains.ponomarevss.sportradarnflnotes.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import moxy.MvpAppCompatActivity
-import moxy.ktx.moxyPresenter
 import org.koin.android.ext.android.inject
 import ru.geekbrains.ponomarevss.sportradarnflnotes.R
 import ru.geekbrains.ponomarevss.sportradarnflnotes.databinding.ActivityMainBinding
 import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.model.navigation.IScreens
-import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.presenter.MainPresenter
 import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.view.MainView
-import ru.geekbrains.ponomarevss.sportradarnflnotes.ui.App
-import ru.geekbrains.ponomarevss.sportradarnflnotes.ui.BackButtonListener
-import javax.inject.Inject
 
 class MainActivity : MvpAppCompatActivity(), MainView {
 
@@ -23,20 +19,19 @@ class MainActivity : MvpAppCompatActivity(), MainView {
     private val navigator = AppNavigator(this, R.id.container)
     private val screens: IScreens by inject()
 
-//    private val presenter by moxyPresenter {
-//        MainPresenter().apply {
-//            App.instance.appComponent.inject(this)
-//        }
-//    }
     private var vb: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        App.instance.appComponent.inject(this)
         vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb?.root)
-        router.replaceScreen(screens.seasons())
+
+        if (savedInstanceState == null) {
+            router.replaceScreen(screens.seasons())
+        }
     }
+
+
 
     override fun onResumeFragments() {
         super.onResumeFragments()
@@ -49,11 +44,6 @@ class MainActivity : MvpAppCompatActivity(), MainView {
     }
 
     override fun onBackPressed() {
-//        supportFragmentManager.fragments.forEach {
-//            if (it is BackButtonListener && it.backPressed()){
-//                return
-//            }
-//        }
-//        presenter.backPressed()
+
     }
 }
--- app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/ui/fragment/SeasonsFragment.kt
+++ app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/ui/fragment/SeasonsFragment.kt
@@ -5,13 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import moxy.MvpAppCompatFragment
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.geekbrains.ponomarevss.sportradarnflnotes.databinding.FragmentSeasonsBinding
-import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.model.entity.general.Season
 import ru.geekbrains.ponomarevss.sportradarnflnotes.ui.BackButtonListener
 import ru.geekbrains.ponomarevss.sportradarnflnotes.ui.adapter.SeasonsRVAdapter
 import ru.geekbrains.ponomarevss.sportradarnflnotes.viewmodel.SeasonsViewModel
@@ -22,9 +18,8 @@ class SeasonsFragment : MvpAppCompatFragment(), BackButtonListener {
     }
 
     private var vb: FragmentSeasonsBinding? = null
-//    private val viewModel: SeasonsViewModel by inject()
-    private val viewModel: SeasonsViewModel by viewModel()
-    private val observer = Observer<List<Season>> { init(it) }
+    private val seasonsViewModel: SeasonsViewModel by viewModel()
+
     var adapter: SeasonsRVAdapter? = null
 
     override fun onCreateView(
@@ -33,30 +28,19 @@ class SeasonsFragment : MvpAppCompatFragment(), BackButtonListener {
         savedInstanceState: Bundle?
     ) = FragmentSeasonsBinding.inflate(inflater, container, false).also {
         vb = it
-    }.root.also { Log.e("AAA", "fragment created") }
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.liveDataForViewToObserve.observe(viewLifecycleOwner, observer)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        seasonsViewModel.liveDataForViewToObserve.observe(viewLifecycleOwner) {}
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         vb = null
-        Log.e("AAA", "fragment destroyed")
     }
-
-    private fun init(seasons: List<Season>) {
-        vb?.rvSeasons?.layoutManager = LinearLayoutManager(context)
-        adapter = SeasonsRVAdapter(seasons)
-//        vb?.rvSeasons?.adapter = adapter
-    }
-
-//    override fun updateList() {
-//        adapter?.notifyDataSetChanged()
-//    }
-
-//    override fun backPressed() = presenter.backPressed()
-
-
 }
