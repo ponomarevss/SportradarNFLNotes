24e3a536f5154f785880ea11e08fb641ebe065ed
ponomarevss <s-siberia@yandex.ru>
2022-09-06 11:31:21 +0500

    di: api module

--- app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/di/DiConstants.kt
+++ app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/di/DiConstants.kt
@@ -2,3 +2,5 @@ package ru.geekbrains.ponomarevss.sportradarnflnotes.di
 
  internal const val NAME_REMOTE = "Remote"
  internal const val NAME_LOCAL = "Local"
+ internal const val NAME_BASE_URL = "https://api.sportradar.us/nfl/official/trial/v7/en/"
+ internal const val NAME_LOGO_URL = "https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/"
--- app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/di/KoinModules.kt
+++ app/src/main/java/ru/geekbrains/ponomarevss/sportradarnflnotes/di/KoinModules.kt
@@ -2,8 +2,15 @@ package ru.geekbrains.ponomarevss.sportradarnflnotes.di
 
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.model.api.IDataSource
 import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.model.navigation.IScreens
 import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.model.repo.ISeasonsRepo
 import ru.geekbrains.ponomarevss.sportradarnflnotes.mvp.model.repo.SeasonsRepo
@@ -20,4 +27,21 @@ val application = module {
 val seasonsFragment = module {
     single<ISeasonsRepo> { SeasonsRepo() }
     viewModel { SeasonsViewModel(get()) }
+}
+
+val api = module {
+    single<Gson> {
+        GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .excludeFieldsWithoutExposeAnnotation()
+            .create()
+    }
+    single<IDataSource> {
+        Retrofit.Builder()
+            .baseUrl(NAME_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(get()))
+            .build()
+            .create(IDataSource::class.java)
+    }
+
 }
